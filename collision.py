a8b088d19d0e5855a8b4f8c3c79e4bda3642b271
Max Heppelmann <maxwell.heppelmann@servitehs.org>
2025-11-07 08:57:33 -0800

    got collision.py to a state where it runs (albeit with bugs). Particles actually move now since vector * scalar doesn't leave side effects anymore

--- collision.py
+++ collision.py
@@ -1,6 +1,9 @@
 import numpy as np
 import simpleguitk as sg
 
+dt: float = 1 / 60
+canvas_height=600
+canvas_width=600
 
 class Vector:
     def __init__(self, x: float, y: float) -> None:
@@ -14,18 +17,23 @@ class Vector:
         return Vector(self.x - other.x, self.y - other.y)
 
     def __mul__(self, other: float) -> "Vector":
-        self.x *= other
-        self.y *= other
-        return self
+        x=self.x*other
+        y=self.y*other
+        newVector=Vector(x,y)
+        return newVector
 
     def get_magnitude(self) -> float:
-        return np.sqrt(self.x ** 2 + self.y ** 2)
+        return np.sqrt(self.x**2 + self.y**2)
 
     def get_squared_magnitude(self) -> float:
-        return self.x ** 2 + self.y ** 2
+        return self.x**2 + self.y**2
 
     def normalize(self) -> "Vector":
         mag: float = self.get_magnitude()
+        if mag==0:
+            self.x=0
+            self.y=0
+            return self
         self.x /= mag
         self.y /= mag
         return self
@@ -36,7 +44,7 @@ class Vector:
 
 
 class Particle:
-    def __init__(self, x: float, y: float, velocity: Vector, radius: float = 100):
+    def __init__(self, x: float, y: float, velocity: Vector=Vector(0,0), radius: float = 100):
         self.position: Vector = Vector(x, y)
         self.velocity: Vector = velocity
         self.radius: float = radius
@@ -44,20 +52,36 @@ class Particle:
     def frameUpdate(self, canvas):
         self.velocity += gravity * dt
         self.updateVelocity()
+        self.solveWorldCollision()
         for particle in Particles:
             if particle != self:
-                if self.checkCollision(particle):
+                if self.checkParticleCollision(particle):
                     self.solveOverlap(particle)
                     Particle.solveVelocityChange(self, particle)
-        canvas.draw_circle(self.position.x, self.position.y, self.radius, 1, "White")
+        canvas.draw_circle((self.position.x, self.position.y), self.radius, 1, "White")
 
     def updateVelocity(self):
         self.position += self.velocity * dt
 
-    def checkCollision(self, other: "Particle"):
-        distance_between_particles_sqr = (self.position - other.position).get_squared_magnitude()
+    def checkParticleCollision(self, other: "Particle"):
+        distance_between_particles_sqr = (
+            self.position - other.position
+        ).get_squared_magnitude()
         return distance_between_particles_sqr < (self.radius + other.radius) ** 2
 
+    def solveWorldCollision(self):
+        if self.position.x<0+self.radius:
+            self.position.x=0+self.radius
+            self.velocity.x*=-1
+        elif self.position.x>600-self.radius:
+            self.position.x=600-self.radius
+            self.velocity.x*=-1
+        if self.position.y<0+self.radius:
+            self.position.y=0+self.radius
+            self.velocity.y*=-1
+        elif self.position.y>600-self.radius:
+            self.position.y=600-self.radius
+            self.velocity.y*=-1
     def updatePosition(self, angle: float, distance: float):
         deltaX: float = np.cos(angle) * distance
         deltaY: float = np.sin(angle) * distance
@@ -96,25 +120,31 @@ class Particle:
         return tangent * dot
 
     @staticmethod
-    def findCollisionTangent(p1: 'Particle', p2: 'Particle') -> Vector:
+    def findCollisionTangent(p1: "Particle", p2: "Particle") -> Vector:
         return Vector(-(p1.position.x - p2.position.x), p1.position.y - p2.position.y)
 
     def findRelativeVelocity(self, other: "Particle") -> Vector:
         return self.velocity - other.velocity
 
 
-frame = sg.create_frame("Collision Sim", 600, 600)
+frame = sg.create_frame("Collision Sim", canvas_width, canvas_height)
 center_point = (300, 300)
 animation_time: float = 0
-dt: float = 1 / 120
-Particles = [Particle(center_point[0] + x, center_point[1] + y, Vector(0, 0), radius=10) for x in range(-100, 100, 20)
-             for y in range(-100, 100, 20)]
+Particles = [
+    Particle(center_point[0] + x, center_point[1] + y, radius=7.5)
+    for x in range(-100, 100, 20)
+    for y in range(-100, 100, 20)
+]
 gravity = Vector(0, 9.8)
 
 
 def drawHandler(canvas):
-    for particle in Particles:
-        particle.frameUpdate(canvas)
+    global animation_time
+    for i in range(len(Particles)):
+        Particles[i].frameUpdate(canvas)
+    animation_time += dt
 
 
+frame.set_canvas_background("black")
 frame.set_draw_handler(drawHandler)
+frame.start()
